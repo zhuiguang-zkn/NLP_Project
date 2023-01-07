ac5745c12c4c080466010c7f8c24dd2988aa1585
520030910170 <1007736246@qq.com>
2023-01-07 03:14:45 +0000

    加入预训练模型

--- model/slu_baseline_tagging.py
+++ model/slu_baseline_tagging.py
@@ -1,24 +1,21 @@
-# coding=utf8
+#coding=utf8
 import torch
 import torch.nn as nn
 import torch.nn.utils.rnn as rnn_utils
 
 
 class SLUTagging(nn.Module):
+
     def __init__(self, config):
         super(SLUTagging, self).__init__()
         self.config = config
         self.cell = config.encoder_cell
+            # config.encoder_cell in ['LSTM', 'GRU', 'RNN']
         self.word_embed = nn.Embedding(config.vocab_size, config.embed_size, padding_idx=0)
-        self.rnn = getattr(nn, self.cell)(
-            config.embed_size,
-            config.hidden_size // 2,
-            num_layers=config.num_layer,
-            bidirectional=True,
-            batch_first=True,
-        )
+        self.rnn = getattr(nn, self.cell)(config.embed_size, config.rnn_hidden_size // 2, num_layers=config.rnn_num_layers, bidirectional=True, batch_first=True)
+            # self.rnn in ['LSTM', 'GRU', 'RNN']
         self.dropout_layer = nn.Dropout(p=config.dropout)
-        self.output_layer = TaggingFNNDecoder(config.hidden_size, config.num_tags, config.tag_pad_idx)
+        self.output_layer = TaggingFNNDecoder(config.rnn_hidden_size, config.num_tags, config.tag_pad_idx, num_layers=config.mlp_num_layers, hidden_size=config.mlp_hidden_size)
 
     def forward(self, batch):
         tag_ids = batch.tag_ids
@@ -27,7 +24,7 @@ class SLUTagging(nn.Module):
         lengths = batch.lengths
 
         embed = self.word_embed(input_ids)
-        packed_inputs = rnn_utils.pack_padded_sequence(embed, lengths, batch_first=True, enforce_sorted=True)
+        packed_inputs = rnn_utils.pack_padded_sequence(embed, lengths, batch_first=True)
         packed_rnn_out, h_t_c_t = self.rnn(packed_inputs)  # bsize x seqlen x dim
         rnn_out, unpacked_len = rnn_utils.pad_packed_sequence(packed_rnn_out, batch_first=True)
         hiddens = self.dropout_layer(rnn_out)
@@ -36,71 +33,79 @@ class SLUTagging(nn.Module):
         return tag_output
 
     def decode(self, label_vocab, batch):
-        """ Trains on one batch.
-
-        Args:
-            label_vocab (LabelVocab): the vocabulary
-            batch (Batch): the batch for training
-
-        Returns:
-            predictions (list[ list[str] ]): each of its items is a list of tags of one sentence in the batch
-            labels (list[ list[str] ]): each of its items is a list of slot-value pairs of one sentence in the batch
-            loss: the loss on this batch (cross entropy loss computed by TaggingFNNDecoder.forward)
-        """        
         batch_size = len(batch)
-        labels = batch.labels  # list of semantics of each sentence
-        prob, loss = self.forward(batch)  # compute the prob of each tag for each word in each sentence
+        labels = batch.labels
+        prob, loss = self.forward(batch)
         predictions = []
-        for i in range(batch_size):  
-            # process the i-th sentence in the batch
-            pred = torch.argmax(prob[i], dim=-1).cpu().tolist()  # the most likely tags for the words
-            # `pred_tuple` contains all slot-value pairs in this sentence.
+        for i in range(batch_size):
+            pred = torch.argmax(prob[i], dim=-1).cpu().tolist()
             pred_tuple = []
-            # For each slot-value pair, `idx_buff` and `tag_buff` contains 
-            # positions and tags of corresponding words.
-            # For this sentence, `pred_tags` contains all tags.
             idx_buff, tag_buff, pred_tags = [], [], []
-            pred = pred[: len(batch.utt[i])]  # discard the padding
-            for idx, tid in enumerate(pred):  
-                # process one word in the sentence
-                tag = label_vocab.convert_idx_to_tag(tid)  # the most likely tag for this word
+            pred = pred[:len(batch.utt[i])]
+            for idx, tid in enumerate(pred):
+                tag = label_vocab.convert_idx_to_tag(tid)
                 pred_tags.append(tag)
-                if (tag == "O" or tag.startswith("B")) and len(tag_buff) > 0:
-                    # add one slot-value pair since it has been collected
-                    slot = "-".join(tag_buff[0].split("-")[1:])
-                    value = "".join([batch.utt[i][j] for j in idx_buff])
+                if (tag == 'O' or tag.startswith('B')) and len(tag_buff) > 0:
+                    slot = '-'.join(tag_buff[0].split('-')[1:])
+                    value = ''.join([batch.utt[i][j] for j in idx_buff])
                     idx_buff, tag_buff = [], []
-                    pred_tuple.append(f"{slot}-{value}")
-                    if tag.startswith("B"):
+                    pred_tuple.append(f'{slot}-{value}')
+                    if tag.startswith('B'):
                         idx_buff.append(idx)
                         tag_buff.append(tag)
-                elif tag.startswith("I") or tag.startswith("B"):
+                elif tag.startswith('I') or tag.startswith('B'):
                     idx_buff.append(idx)
                     tag_buff.append(tag)
             if len(tag_buff) > 0:
-                # add the last slot-value pair
-                slot = "-".join(tag_buff[0].split("-")[1:])
-                value = "".join([batch.utt[i][j] for j in idx_buff])
-                pred_tuple.append(f"{slot}-{value}")
+                slot = '-'.join(tag_buff[0].split('-')[1:])
+                value = ''.join([batch.utt[i][j] for j in idx_buff])
+                pred_tuple.append(f'{slot}-{value}')
             predictions.append(pred_tuple)
         return predictions, labels, loss.cpu().item()
 
 
+"""
 class TaggingFNNDecoder(nn.Module):
-    """ Contains a fully connected layer and a softmax layer.
-    Input: a batch of hidden vectors of RNN, 
-    whose shape is 'size of batch' x 'length of the longest sequence' x 'dim of hidden vector'.
-    Output: a batch of probabilities of tags, 
-    whose shape is 'size of batch' x 'length of the longest sequence' x 'num of tags`.
-    """    
+
     def __init__(self, input_size, num_tags, pad_id):
         super(TaggingFNNDecoder, self).__init__()
         self.num_tags = num_tags
         self.output_layer = nn.Linear(input_size, num_tags)
         self.loss_fct = nn.CrossEntropyLoss(ignore_index=pad_id)
-
+    
     def forward(self, hiddens, mask, labels=None):
         logits = self.output_layer(hiddens)
+        logits += (1 - mask).unsqueeze(-1).repeat(1, 1, self.num_tags) * -1e32
+        prob = torch.softmax(logits, dim=-1)
+        if labels is not None:
+            loss = self.loss_fct(logits.view(-1, logits.shape[-1]), labels.view(-1))
+            return prob, loss
+        return prob
+"""
+
+
+class TaggingFNNDecoder(nn.Module):
+
+    def __init__(self, input_size, num_tags, pad_id, num_layers=1, hidden_size=256):
+        super(TaggingFNNDecoder, self).__init__()
+        self.num_tags = num_tags
+        
+        self.linear_layers = nn.ModuleList()
+
+        if num_layers >= 2:
+            self.linear_layers.append(nn.Linear(input_size, hidden_size))   # input layer
+            for _ in range(1, num_layers):
+                self.linear_layers.append(nn.Linear(hidden_size, hidden_size))   # hidden layer
+            self.linear_layers.append(nn.Linear(hidden_size, num_tags))   # output layer
+        else:
+            self.linear_layers.append(nn.Linear(input_size, num_tags))   # output layer
+
+        self.loss_fct = nn.CrossEntropyLoss(ignore_index=pad_id)
+    
+    def forward(self, hiddens, mask, labels=None):
+        logits = hiddens
+        for _, layer in enumerate(self.linear_layers):
+            logits = layer(logits)
         logits += (1 - mask).unsqueeze(-1).repeat(1, 1, self.num_tags) * -1e32
         prob = torch.softmax(logits, dim=-1)
         if labels is not None:
--- utils/example.py
+++ utils/example.py
@@ -1,57 +1,85 @@
 import json
-
 from utils.vocab import Vocab, LabelVocab
 from utils.word2vec import Word2vecUtils
+from utils.bert2vec import BertUtils, BERT_WWM_EXT, Roberta
 from utils.evaluator import Evaluator
 
 class Example():
-    """ Object for describing an example.
-    E.g., if `obj = Example(ex: dict)`, where
-    
-    `ex = {'utt_id': 1, 'manual_transcript': '我要去新光北路', 'asr_1best': '我要去新光北路', 'semantic': [['inform', '终点名称', '新光北路']]},`
-
-    then
-    + `obj.utt = '我要去新光北路'`, 
-    + `obj.slot = {'inform-终点名称': '新光北路'}`, 
-    + `obj.tags = ['O', 'O', 'O', 'B-inform-终点名称', 'I-inform-终点名称', 'I-inform-终点名称', 'I-inform-终点名称']`,
-    + `obj.slotvalue = ['inform-终点名称-新光北路']`,
-    + `obj.input_idx = [21, 40, 41, 60, 229, 151, 83]` (indices of words),
-    + `obj.tag_id = [1, 1, 1, 14, 15, 15, 15]` (indices of tags of words).
-
-    Also, the class method `load_dataset` returns a list of `Example` objects from dataset, and the class method 
-    `configuration` initializes objects of `Evaluator`, `Vocab`, `Word2vecUtils` and `LabelVocab` as class attributes.
-    """    
 
     @classmethod
-    def configuration(cls, root, train_path=None, word2vec_path=None):
+    def configuration(cls, vocab_path=None, ontology_path=None, word2vec_path=None, 
+                        spoken_language_select = 'asr_1best', word_embedding = 'Word2vec'):
         cls.evaluator = Evaluator()
-        cls.word_vocab = Vocab(padding=True, unk=True, filepath=train_path)
-        cls.word2vec = Word2vecUtils(word2vec_path)
-        cls.label_vocab = LabelVocab(root)
+        cls.word_vocab = Vocab(padding=True, unk=True, vocab_path=vocab_path,
+                                spoken_language_select=spoken_language_select)
+            # load train vocabularies
 
+
+        if word_embedding == 'Word2vec' :
+            cls.word2vec = Word2vecUtils(word2vec_path)  
+        elif word_embedding == 'Bert' :
+            cls.word2vec = BertUtils(None)
+        elif word_embedding == 'WWM':
+            cls.word2vec=BERT_WWM_EXT(None)
+        elif word_embedding == 'Roberta':
+            cls.word2vec=Roberta(None)
+            
+        cls.label_vocab = LabelVocab(ontology_path)
+            # used to determine the number of tags and the category of tags
+            # there is 2(B/I) * 2(acts: inform/deny) * 18(slots: poi名称, poi修饰, ...) + 2(O/<pad>) = 74 kinds of tags
+    
     @classmethod
-    def load_dataset(cls, data_path):
-        """ Returns a list of `Example` objects from dataset.
+    def load_dataset(cls, data_path, spoken_language_select='asr_1best'):
         """
-
-        datas = json.load(open(data_path, 'r', encoding='utf-8'))
+        cls : Example()
+        data_path : path(es) of dataset, str | list([str, str, ...])
+        spoken_language_select : str | list([str, str]), str in ['asr_1best', 'manual_transcript']
+        """
+        if isinstance(data_path, str):
+            data_path = [data_path]
+        if isinstance(spoken_language_select, str):
+            spoken_language_select = [spoken_language_select]
+        
         examples = []
-        for data in datas:
-            for utt in data:
-                ex = cls(utt)
-                examples.append(ex)
+        for sls in spoken_language_select:
+            # assert spoken_language_select in ['asr_1best', 'manual_transcript']
+            assert sls in ['asr_1best', 'manual_transcript']
+            for path in data_path:
+                datas = json.load(open(path, 'r'))
+                for data in datas:
+                    for utt in data:
+                        ex = cls(utt, sls)
+                        examples.append(ex)
+            
         return examples
 
-    def __init__(self, ex: dict):
+
+    def __init__(self, ex: dict, spoken_language_select):
         super(Example, self).__init__()
         self.ex = ex
-
-        self.utt = ex['asr_1best']
+        """
+        ex['utt_id'] : 对话的轮数
+        ex['manual_transcript'] : 人工修正过的无噪音的输入
+        ex['asr_1best'] : ASR 模型识别出来的结果
+        ex['semantic'] : 语义标签
+        For example (datas[0][0]), 
+            {'utt_id': 1,
+            'manual_transcript': '给我导航云南省昆明市黄土坡',
+            'asr_1best': '导航云南省昆明市黄土坡',
+            'semantic': [['inform', '操作', '导航'], ['inform', '终点名称', '云南省昆明市黄土坡']]}
+        """
+        # self.utt = ex['asr_1best']
+        # self.utt = ex['manual_transcript']
+        # assert spoken_language_select in ['asr_1best', 'manual_transcript']
+        self.utt = ex[spoken_language_select]
+        
         self.slot = {}
-        for label in ex['semantic']:
-            act_slot = f'{label[0]}-{label[1]}'
-            if len(label) == 3:
-                self.slot[act_slot] = label[2]
+        if 'semantic' in ex:
+            for label in ex['semantic']:
+                act_slot = f'{label[0]}-{label[1]}'
+                if len(label) == 3:
+                    self.slot[act_slot] = label[2]
+        
         self.tags = ['O'] * len(self.utt)
         for slot in self.slot:
             value = self.slot[slot]
@@ -59,7 +87,38 @@ class Example():
             if bidx != -1:
                 self.tags[bidx: bidx + len(value)] = [f'I-{slot}'] * len(value)
                 self.tags[bidx] = f'B-{slot}'
+        
         self.slotvalue = [f'{slot}-{value}' for slot, value in self.slot.items()]
-        self.input_idx = [Example.word_vocab[c] for c in self.utt]
-        l = Example.label_vocab
-        self.tag_id = [l.convert_tag_to_idx(tag) for tag in self.tags]
+        
+        self.input_idx = [Example.word_vocab[c] for c in self.utt]  
+            # `Example.word_vocab.word2id : dict` turn vocab into index
+                # {'<pad>': 0, '<unk>': 1, '导': 2, '航': 3, '云': 4, '南': 5, '省': 6, '昆': 7, ...}
+            # len(Example.word_vocab.word2id) == 1782 when 
+                # spoken_language_select = 'asr_1best'
+                # and use train.json only
+        self.tag_id = [Example.label_vocab.convert_tag_to_idx(tag) for tag in self.tags]
+            # `Example.label_vocab.tag2idx : dict` turn tag into index
+        """
+        Output is stored in self.utt(vocab seq), self.tags(tag seq), self.input_idx(vocab seq index), self.tag_id(tag seq index)
+        For example (datas[0][0]),
+            self.utt = '导航云南省昆明市黄土坡'
+            self.slot = {'inform-操作': '导航', 'inform-终点名称': '云南省昆明市黄土坡'}
+            self.tags = [   'B-inform-操作',
+                            'I-inform-操作',
+                            'B-inform-终点名称',
+                            'I-inform-终点名称',
+                            'I-inform-终点名称',
+                            'I-inform-终点名称',
+                            'I-inform-终点名称',
+                            'I-inform-终点名称',
+                            'I-inform-终点名称',
+                            'I-inform-终点名称',
+                            'I-inform-终点名称'     ]
+            self.slotvalue = ['inform-操作-导航', 'inform-终点名称-云南省昆明市黄土坡']
+            self.input_idx = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+            self.tag_id = [30, 31, 14, 15, 15, 15, 15, 15, 15, 15, 15]
+        """
+
+
+    def __str__(self):
+        return f"vocab seq: {self.utt}\ntag seq: {self.tags}\nvocab seq(index): {self.input_idx}\ntag seq(index): {self.tag_id}"
